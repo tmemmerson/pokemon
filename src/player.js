2bb83efe0af606889a0e7396e839740a1f3a7f0e
Tristan Emmerson <tristan@stickerslug.com>
2020-06-25 13:11:55 -0700

    RED refactor.

--- src/player.js
+++ src/player.js
@@ -3,7 +3,11 @@ let Robot = function () {
 }
 
 Robot.prototype.status = function() {
-    return "off"
+  return this._status
 }
+
+Robot.prototype.power = function() {
+  this._status = "on"
+};
 
 module.exports = Robot
